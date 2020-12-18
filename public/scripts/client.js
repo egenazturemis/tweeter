d8bbab3af92a30c51b97c74bcb46edf502432613
Egenaz <egenazturemis@gmail.com>
2020-12-18 08:06:54 +0000

    final commit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,20 +1,19 @@
-/*
-* Client-side JS logic goes here
-* jQuery is already loaded
-* Reminder: Use (and do all your DOM work in) jQuery's document ready function
-*/
+// Prevent XSS
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+
+};
 
 const renderTweets = function(tweets) {
-  // loops through tweets
   for (let tweet of tweets) {
-    // calls createTweetElement for each tweet
     let tweetElement = createTweetElement(tweet);
-    // takes return value and appends it to the tweets container
     $('#tweets-container').prepend(tweetElement);
-  }
-}
+  };
 
-// return html
+};
+
 const createTweetElement = function(tweet) {
   const $tweet = `<article class="tweet">
   <header class="tweet-header">
@@ -28,15 +27,15 @@ const createTweetElement = function(tweet) {
   <footer class="tweet-footer">
     <p class="timestamp">10 days ago</p>
     <div class="tweetIcons">
-      <img src="/images/flags.png" width="20" height="20">
-      <img src="/images/retweet.png" width="20" height="20">
-      <img src="/images/like.png" width="20" height="20">
+      <img id="i1" src="/images/flag.png" width="15" height="15">
+      <img id="i2" src="/images/retweet.png" width="18" height="18">
+      <img id="i3" src="/images/like.png" width="15" height="15">
     </div>
   </footer>
 </article>`
-  
   return $tweet;
-}
+
+};
 
 
 const loadTweets = function () {
@@ -49,27 +48,18 @@ const loadTweets = function () {
   })
   .catch((err) => {
     console.log(err);
-  })
+  });
+
 };
 
-// prevent xss:
-const escape =  function(str) {
-  let div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-}
 
-/////////////////////////////////////////////////////
 $(document).ready(function() {
-
   loadTweets();
-
+  
   $('.submit-tweet').submit(function(event) {
     event.preventDefault();
-    
     let tweetBox = $('#tweet-text').val()
     
-    // prevent empty or too long tweets from being posted:
     if (tweetBox.length > 140) {
       const alert = "<p>You've exceeded the max. character limit of 140 characters!</p>";
       $('.throwError').append(alert);
@@ -93,8 +83,7 @@ $(document).ready(function() {
       })
       .catch((err) => {
         console.log(err);
-      })
-      
-    }
-  })
+      });
+    };
+  });
 });
